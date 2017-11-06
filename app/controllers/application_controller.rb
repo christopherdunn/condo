5bdb00b90c031790aedac7a30ed8f9dffb513fc9
Chris Dunn <cdunn@backbayrealtygroup.com>
2017-11-06 14:41:34 -0500

    added devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-skip_before_action :verify_authenticity_token
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  skip_before_action :verify_authenticity_token
 
 protected
-
+def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :phone])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :phone])
+  end
 
 end

7d77077c367c5e284541d6068732b7e3faa6e2e9
Chris Dunn <cdunn@backbayrealtygroup.com>
2017-11-10 17:53:59 -0500

    aws settings

--- app/controllers/condo_documents_controller.rb
+++ app/controllers/condo_documents_controller.rb
@@ -1,7 +1,38 @@
 class CondoDocumentsController < ApplicationController
 before_action :authenticate_user!
   def index
-    @condo_documents = CondoDocument.where(hoa_id: params[:id])
+    @hoa = Hoa.find(params[:hoa_id])
+
+    @condo_documents = CondoDocument.where(hoa_id: params[:hoa_id])
+    
   end
+
+  def new
+
+    @hoa = Hoa.find(params[:hoa_id])
+    @condo_document = CondoDocument.new
+  end
+
+  # POST /articles
+  def create
+    @hoa = Hoa.find(params[:hoa_id])
+    @condo_document = CondoDocument.new(doc_params)
+    @condo_document.hoa = @hoa
+
+    if @condo_document.save
+      flash[:notice] = 'Document was successfully submited.'
+      redirect_to hoa_condo_documents_path
+    else
+      render action: 'new'
+    end
+  end
+
+  private
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def doc_params
+    params.require(:condo_document).permit(:document, :name, hoa_id: Hoa.find(params[:hoa_id]))
+  end
+
 
 end
--- app/uploaders/document_uploader.rb
+++ app/uploaders/document_uploader.rb
@@ -4,9 +4,15 @@ class DocumentUploader < CarrierWave::Uploader::Base
   # include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
 
+
   # Choose what kind of storage to use for this uploader:
-  storage :file
-  # storage :fog
+  if Rails.env.test?
+      storage :file
+    else
+      storage :fog
+    end
+
+
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
@@ -36,9 +42,9 @@ class DocumentUploader < CarrierWave::Uploader::Base
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  # def extension_whitelist
-  #   %w(jpg jpeg gif png)
-  # end
+   def extension_whitelist
+    %w(pdf jpg jpeg gif png doc docx xls xlsx)
+   end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
